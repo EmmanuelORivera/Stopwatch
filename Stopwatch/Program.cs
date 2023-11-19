3a421a1f63567f72cd84473cee43b8996f6a4301
Emmanuel Ochoa <eorivera95@gmail.com>
2023-11-19 16:49:48 -0600

    refactor: Stopwatch file in his own file

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -1,39 +1,6 @@
 ﻿using System;
 namespace Stopwatch
 {
-	public static class Stopwatch
-	{
-		static private DateTime _startTime;
-		static private bool _started = false;
-		static public void Start()
-		{
-			if (_started) throw new InvalidOperationException("Stopwatch started twice");
-
-			if (_startTime == DateTime.MinValue)
-			{
-				_startTime = DateTime.Now;
-                Console.WriteLine("Stopwatch Started At: {0}",TimeSpan.Zero);
-            }
-			else
-			{
-                Console.WriteLine("Stopwatch Resumed At: {0}", GetElapsedTime());
-            }
-				
-			_started = true;
-		}
-
-		public static TimeSpan GetElapsedTime()
-		{
-			return DateTime.Now - _startTime;
-		}
-
-		static public TimeSpan Stop()
-		{
-			_started = false;
-
-			return GetElapsedTime();
-		}
-	}
 	internal class Program
 	{
 		static void Main(string[] args)
